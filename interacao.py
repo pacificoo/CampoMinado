2299e6c8ebb700d68ddba8f09f0d472b1e20c721
julianaolive <121642466+julianaolive@users.noreply.github.com>
2022-12-29 18:41:12 -0300

    Update interacao.py

--- interacao.py
+++ interacao.py
@@ -1,29 +1,56 @@
 from analise import Analise
+#acho que preciso importar do log tbm 
 
-class Interacao:
+class interacao:
 
-    def regras_do_jogo():
+    def __init__(self):
+        pass
+        
+
+    def regras(self):
         ''' funcao que printa as regras do jogo para usario caso seja pedido
         none->none'''
-        pass
+        print('O objetivo do jogo é descobrir as casas vazias. A cada casa vazia descoberta, o número de bombas ao seu redor é revelado')
+        print('O jogador perde se a casa escolhida conter uma bomba')
 
-    def Tela_pause():
+    def Tela_pause(self):
         '''função em que o usuario pode escolher entre as seguintes opções: Voltar
 para o jogo(1), salvar o jogo em outro arquivo(2), voltar para o menu principal(3) e
 sair do jogo(4).
         none->none'''
-        pass
+        print('--------Tela de Pause----------')
+        print('Para voltar para o jogo aperte 1\nPara recarregar um jogo aperte 2\nPara voltar ao menu inicial aperte 3\nPara sair do jogo aperte 4')
+        while True:
+            num = input('Digite o número escolhido ')
+            if not( len(num)!=1 or num[0] not in '1234'):
+                break
+            print('O numero não é válido ou o valor digitado não é um número')
+   
+        if int(num) == 1:
+            self.retorno()
+        if int(num) == 2:
+            self.carregar_jogo()
+        if int(num)==3:
+            self.menu_Principal()
+        if int(num) == 4:
+            self.salvar()
+    
+        #perguntar para o andre se precisa raise erro
 
     def posicao():
         '''função que pede a ao usuário posicao
         none->none'''
-        pass
+        posicao = input('Digite a posição escolhida,separado por vírgulas')
+        pos1 = int(posicao[0])
+        pos2 = int(posicao[2])
+
+        return pos1, pos2
+
 
     def retorno():
         '''funcao que retorna o jogo em andamento
         none->none'''
         pass
-
 
     def salvar(arquivo):
         '''funcao que pergunta ao usuário se ele deseja salvar ou não o jogo
@@ -32,8 +59,8 @@ sair do jogo(4).
     
     def carregar_jogo(arquivo):
         '''funçao que seleciona um jogo salvo anteriormente
-        none->none'''
-        pass
+        str->none'''
+        nome = input('Digite o nome do jogo que deseja abrir com a sua respectiva extensao'
 
     
     def novo_jogo():
@@ -41,16 +68,37 @@ sair do jogo(4).
         none->none'''
         pass
 
-    def menu_Principal():
+    def menu_Principal(self):
         '''funcao que escolhe entre as seguintes opções: Novo Jogo (1), carregar jogo
         (2), estatísticas (3) e regras dos jogo (4)
-        none->none'''
-        pass
+        interacao->none'''
+        print('--------Menu Principal----------')
+        print('Para criar um Novo Jogo aperte 1\nPara carregar um jogo aperte 2\nPara ver as estatisticas aperte 3\nPara ver as regras do jogo aperte 4')
+        while True:
+            num = input('Digite o número escolhido ')
+            if not( len(num)!=1 or num[0] not in '1234'):
+                break
+            print('O numero não é válido ou o valor digitado não é um número')
+   
+        if int(num) == 1:
+            self.novo_jogo()
+        if int(num) == 2:
+            self.carregar_jogo()
+        if int(num)==3:
+            self.estatisticas()
+        if int(num) == 4:
+            self.regras()
+            
+        
 
     def estatisticas(dados):
         '''funcao que printa para o usuario as estatisticas dos jogos anteriores'''
         pass
-        
+        #importar de outras funcões 
+
+class NumError(Exception):
+    pass
+
 
     
 
