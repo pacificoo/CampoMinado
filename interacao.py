b7b3f0fe5942d4a25cfc563ccaeac17803e6afd7
julianaolive <121642466+julianaolive@users.noreply.github.com>
2022-12-29 18:58:01 -0300

    Update interacao.py

--- interacao.py
+++ interacao.py
@@ -12,6 +12,7 @@ class interacao:
         none->none'''
         print('O objetivo do jogo é descobrir as casas vazias. A cada casa vazia descoberta, o número de bombas ao seu redor é revelado')
         print('O jogador perde se a casa escolhida conter uma bomba')
+    
 
     def Tela_pause(self):
         '''função em que o usuario pode escolher entre as seguintes opções: Voltar
@@ -20,6 +21,7 @@ sair do jogo(4).
         none->none'''
         print('--------Tela de Pause----------')
         print('Para voltar para o jogo aperte 1\nPara recarregar um jogo aperte 2\nPara voltar ao menu inicial aperte 3\nPara sair do jogo aperte 4')
+
         while True:
             num = input('Digite o número escolhido ')
             if not( len(num)!=1 or num[0] not in '1234'):
@@ -40,7 +42,7 @@ sair do jogo(4).
     def posicao():
         '''função que pede a ao usuário posicao
         none->none'''
-        posicao = input('Digite a posição escolhida,separado por vírgulas')
+        posicao = input('Digite a posição escolhida separada por vírgulas')
         pos1 = int(posicao[0])
         pos2 = int(posicao[2])
 
@@ -60,7 +62,7 @@ sair do jogo(4).
     def carregar_jogo(arquivo):
         '''funçao que seleciona um jogo salvo anteriormente
         str->none'''
-        nome = input('Digite o nome do jogo que deseja abrir com a sua respectiva extensao'
+        nome = input('Digite o nome do jogo que deseja abrir com a sua respectiva extensao')
 
     
     def novo_jogo():
@@ -70,26 +72,29 @@ sair do jogo(4).
 
     def menu_Principal(self):
         '''funcao que escolhe entre as seguintes opções: Novo Jogo (1), carregar jogo
-        (2), estatísticas (3) e regras dos jogo (4)
+        (2), estatísticas (3), regras dos jogo (4), e sair do jogo (5)
         interacao->none'''
-        print('--------Menu Principal----------')
-        print('Para criar um Novo Jogo aperte 1\nPara carregar um jogo aperte 2\nPara ver as estatisticas aperte 3\nPara ver as regras do jogo aperte 4')
+
         while True:
-            num = input('Digite o número escolhido ')
-            if not( len(num)!=1 or num[0] not in '1234'):
-                break
-            print('O numero não é válido ou o valor digitado não é um número')
-   
-        if int(num) == 1:
-            self.novo_jogo()
-        if int(num) == 2:
-            self.carregar_jogo()
-        if int(num)==3:
-            self.estatisticas()
-        if int(num) == 4:
-            self.regras()
-            
-        
+            print('\n--------Menu Principal----------\n')
+            print('Para criar um Novo Jogo aperte 1\nPara carregar um jogo aperte 2\nPara ver as estatisticas aperte 3\nPara ver as regras do jogo aperte 4\nPara sair do jogo aperte 5')
+            while True:
+                num = input('Digite o número escolhido ')
+                if not( len(num)!=1 or num[0] not in '12345'):
+                    break
+                print('O numero não é válido ou o valor digitado não é um número')
+                
+                
+            if int(num) == 1:
+                self.novo_jogo()
+            if int(num) == 2:
+                self.carregar_jogo()
+            if int(num)==3:
+                self.estatisticas()
+            if int(num) == 4:
+                self.regras()
+            if int(num) ==5:
+                break  
 
     def estatisticas(dados):
         '''funcao que printa para o usuario as estatisticas dos jogos anteriores'''
